69dee8ad4be0e65b920f7021e0009f09522097f9
alishakor <aliuokeowo@gmail.com>
2023-05-30 00:33:50 +0100

    added quotations

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,11 +14,11 @@ def status():
 def stats():
     """Retrieve the number of each object by type"""
     stats = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
     }
     return jsonify(stats)
