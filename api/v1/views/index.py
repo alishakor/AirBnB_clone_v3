d6709b0123d1f0a547b48c33b354b45c93cf7f6b
alishakor <113800450+alishakor@users.noreply.github.com>
2023-05-29 01:16:32 +0100

    status api

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """return route to the status api"""
+    return jsonify("status": "OK")
