6ed281f8798a0afa023f11f94a7948f1d284777f
alishakor <aliuokeowo@gmail.com>
2023-05-30 02:04:06 +0100

    executable files and other folders

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""This module defines a route for stats"""
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
-    """return route to the status api"""
+    """Return a JSON with status: OK"""
     return jsonify({"status": "OK"})
 
 
@@ -14,11 +22,11 @@ def status():
 def stats():
     """Retrieve the number of each object by type"""
     stats = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(stats)
